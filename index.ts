f3bab54d82df6a257627177331b213cdfc0f9bc4
Brad <bradtaniguchi@gmail.com>
2021-10-18 22:31:56 +0000

    Update existing tests, passing

--- index.ts
+++ index.ts
@@ -3,30 +3,59 @@
  * casted to strings, which could create issues if the data
  * passed is inconsistent.
  */
-type UnformattedData = any[][];
+type UnformattedData = Array<Array<any>>;
 
 /**
  * Format options that can be passed.
  */
 type FormatOptions = {
   /**
-   * This can be used to provide a list of headers, 
+   * This can be used to provide a list of headers,
    * rather than passing them in as the first row of
-   * unformatted text. 
+   * unformatted text.
    */
-  headers?: any[];
+  headers?: any[],
   /**
    * The column delimiter character(s), defaults to `|`.
    * This character(s) is automatically padded with spaces
    * on either side automatically.
    */
-  columnDelimiter?: string;
+  columnDelimiter?: string,
   /**
    * The row delimiter character(s), defaults to `-`.
    */
-  rowDelimiter?: string;
+  rowDelimiter?: string,
 };
 /**
  * Formats text into a table object.
  */
-export const formatTextToTable = (data: UnformattedData, options?: FormatOptions) => '';
+export const formatTextToTable = (
+  data: UnformattedData,
+  options?: FormatOptions
+) => {
+  // alias for simplicity
+  const rows = data;
+  const toStr = (val: any) => "" + val;
+  const headers = options?.headers || data[0] || [];
+  const columnDelimiter = options?.columnDelimiter || "|";
+  const rowDelimiter = options?.rowDelimiter || "-";
+
+  const baseColumnWidths = headers.map((header) => toStr(header).length);
+  const columnWidths = rows.reduce((acc, row) => {
+    row.forEach((column, index) => {
+      const currentColumnLength = toStr(column).length
+      if (currentColumnLength > acc[index]) {
+        acc[index] = currentColumnLength;
+      }
+    });
+    return acc;
+  }, baseColumnWidths) as number[];
+
+  const dataStr = rows
+    .map((row) =>
+      row.map((column, index) => toStr(column).padEnd(columnWidths[index], " ")).join(` ${columnDelimiter} `)
+    );
+  const headersStr = headers.join(` ${columnDelimiter} `);
+  
+  return [headersStr, ...dataStr].filter((_ ) =>_).join('\n');
+};
--- tests.test.ts
+++ tests.test.ts
@@ -1,19 +1,43 @@
-const { formatTextToTable } = require('./index');
+const { formatTextToTable } = require("./index");
 
-describe('formatTextToTable', () => {
-  test('is defined', () => {
+describe("formatTextToTable", () => {
+  test("is defined", () => {
     expect(formatTextToTable).toBeTruthy();
-    expect(typeof formatTextToTable).toEqual('function');
+    expect(typeof formatTextToTable).toEqual("function");
   });
-  test('given empty array returns empty string', 
-    () => expect(formatTextToTable([])).toEqual(''));
-  test('given 2d empty array, returns empty string', 
-    () => expect(formatTextToTable([[]])).toEqual(''));
-  test('given 2d empty array, with seperate defined headers, returns headers', () => 
-    () => expect(formatTextToTable([], {
-      headers: ['one', 'two']
-    })).toEqual('one | two'));
-  test.todo('given 2d empty array, with empty seperate defined headers, returns empty string');
-  test.todo('given 2d array with data with "long" headers, display table');
-  test.todo('given 2d array with data with "long" data, display table with expanded headers');
+  test("given empty array returns empty string", () =>
+    expect(formatTextToTable([])).toEqual(""));
+  test("given 2d empty array, returns empty string", () =>
+    expect(formatTextToTable([[]])).toEqual(""));
+  test("given 2d empty array, with seperate defined headers, returns headers", () =>
+    expect(
+      formatTextToTable([], {
+        headers: ["one", "two"],
+      })
+    ).toEqual("one | two"));
+  // TODO: add tests to check for custom column delimiters
+  test("given 2d empty array, with empty seperate defined headers, returns empty string", () =>
+    expect(
+      formatTextToTable([[]], {
+        headers: [],
+      })
+    ).toEqual(""));
+  test('given 2d array with data with "long" headers, display table', () =>
+    expect(
+      formatTextToTable(
+        [
+          ["brad", "100"],
+          ["foo", "bar"],
+        ],
+        {
+          headers: ["name", "aggeeeeeeeee"],
+        }
+      )
+    ).toEqual(
+      'name | aggeeeeeeeee\n' + 
+      'brad | 100         \n' +
+      'foo  | bar         '));
+  test.todo(
+    'given 2d array with data with "long" data, display table with expanded headers'
+  );
 });
