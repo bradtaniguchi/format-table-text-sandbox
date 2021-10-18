aaa075f715c7bfebd469ab38b5471a0a2cff0c9e
Brad <bradtaniguchi@gmail.com>
2021-10-18 21:42:17 +0000

    Add README, and other tests

---
+++ README.md
@@ -0,0 +1,7 @@
+#Format Table Text Sandbox
+
+This repo is just a backup of sandbox tests to build a basic text formatting
+utility to format a 2d array as a string.
+
+Replit:
+https://replit.com/@bradtaniguchi/format-table-text#README.md
--- tests.test.ts
+++ tests.test.ts
@@ -5,10 +5,14 @@ describe('formatTextToTable', () => {
     expect(formatTextToTable).toBeTruthy();
     expect(typeof formatTextToTable).toEqual('function');
   });
-  test('returns string', () => expect(formatTextToTable([])).toEqual(''));
-  test.todo('given empty array returns empty string');
-  test.todo('given 2d empty array, returns empty string');
-  test.todo('given 2d empty array, with seperate defined headers, returns headers');
+  test('given empty array returns empty string', 
+    () => expect(formatTextToTable([])).toEqual(''));
+  test('given 2d empty array, returns empty string', 
+    () => expect(formatTextToTable([[]])).toEqual(''));
+  test('given 2d empty array, with seperate defined headers, returns headers', () => 
+    () => expect(formatTextToTable([], {
+      headers: ['one', 'two']
+    })).toEqual('one | two'));
   test.todo('given 2d empty array, with empty seperate defined headers, returns empty string');
   test.todo('given 2d array with data with "long" headers, display table');
   test.todo('given 2d array with data with "long" data, display table with expanded headers');
